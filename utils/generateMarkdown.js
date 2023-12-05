fbe4f5b459b2226d7b35ef790f864b54496c7e52
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-05 11:22:49 -0500

    created template for markdown in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,7 +5,46 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [License](#license)
+ - [Contributing](#contributing)
+ - [Tests](#tests)
+ - [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+GitHub: ${data.github}
+Email: ${data.email}
 
 `;
 }
